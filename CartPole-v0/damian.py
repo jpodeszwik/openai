8c3dd69a87665f040e16221b3a094d83cb8793d7
damienix <damienix1@gmail.com>
2016-05-28 17:47:49 +0200

    Big improvements mostly due to less randomness.

--- CartPole-v0/damian.py
+++ CartPole-v0/damian.py
@@ -1,49 +1,62 @@
-import random
+from random import random
 
 import gym
+from pybrain import LinearLayer
 from pybrain.datasets import SupervisedDataSet
-from pybrain.structure.modules.tanhlayer import TanhLayer
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.tools.shortcuts import buildNetwork
 
-env = gym.make('CartPole-v0')
-net = buildNetwork(4, 4, 1, bias=True, outputbias=False, outclass=TanhLayer)
+
+def print_net(n):
+    for mod in n.modules:
+        for conn in n.connections[mod]:
+            print conn
+            for cc in range(len(conn.params)):
+                print conn.whichBuffers(cc), conn.params[cc]
 
 
 class Trainer:
     def __init__(self):
-        pass
-
-    best_all = 0
+        self.best_all = 0
 
     def run(self):
-        for i_episode in range(10):
+        for i_episode in range(100):
             self.run_episode()
 
     def run_episode(self):
-        best_t, best_set = self.run_try_with_random(False)
+        best_t, best_set = self.run_try_with_random(rand_chance=0)
 
-        for i in range(100):
-            t, ds = self.run_try_with_random()
+        for i in range(20):
+            t, ds = self.run_try_with_random(1.0 / best_t)
             if t > best_t:
                 best_t, best_set = t, ds
 
-        trainer = BackpropTrainer(net, best_set)
+        self.run_try_with_random(0, True)
+
+        if best_t > self.best_all:
+            self.best_all = best_t
+
+        # trainer = BackpropTrainer(net, best_set, lrdecay=0.9, momentum=0.3, weightdecay=0.01, learningrate=0.01)
+        trainer = BackpropTrainer(net, best_set, learningrate=0.1)
+        trainer.trainUntilConvergence(maxEpochs=20, validationProportion=0.1)
+        # trainer.trainUntilConvergence(maxEpochs=20, continueEpochs=5, validationProportion=0.5)
         trainer.train()
 
+        print_net(net)
+
     @staticmethod
-    def run_try_with_random(rand=True):
-        max_frames = 1000
-        ds = SupervisedDataSet(4, 1)
+    def run_try_with_random(rand_chance=0.01, render=False):
+        max_frames = 100000
+        ds = SupervisedDataSet(env_size, 1)
         observation = env.reset()
         for t in range(max_frames):
-            # env.render()
+            if render:
+                env.render()
             # print(observation)
 
-            action = 0 if net.activate(observation)[0] > 0 else 1
+            action = 0 if net.activate(observation)[0] < 0 else 1
 
-            # randomize here
-            if rand and random.random() < 0.1:
+            if random() < rand_chance:
                 action = (action + 1) % 1
 
             ds.addSample(observation, (action,))
@@ -58,5 +71,9 @@ class Trainer:
 
         return t, ds
 
+
+env = gym.make('CartPole-v0')
+env_size = 4
+net = buildNetwork(env_size, 1, bias=True, outputbias=False, outclass=LinearLayer)
 
 Trainer().run()
