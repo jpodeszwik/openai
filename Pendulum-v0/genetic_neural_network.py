31cd85fdcac2921f8baab009213ab60250239960
Jakub Podeszwik <jakub.podeszwik@grupawp.pl>
2016-05-28 17:59:55 +0200

    Pendulum-v0 - code reformat

--- Pendulum-v0/genetic_neural_network.py
+++ Pendulum-v0/genetic_neural_network.py
@@ -1,15 +1,15 @@
-import gym
-import time
-import random
 import copy
+import gym
 import numpy
-from gym.spaces import Box
+import random
+
 
 class GeneticNetworkHelper:
+    @staticmethod
     def crossing(net1, net2):
         crossing_point = random.randint(1, 3)
         new_weights = []
-        
+
         for i in range(crossing_point):
             new_weights.append(net1.weights[i])
 
@@ -18,21 +18,24 @@ class GeneticNetworkHelper:
 
         return Network(new_weights)
 
+    @staticmethod
     def mutate(net):
         mutations = random.randint(1, 3)
         mutated_genes = random.sample([0, 1, 2, 3], mutations)
         new_weights = copy.copy(net.weights)
-        
+
         for idx in mutated_genes:
-            new_weights[idx] = random.random()*2 - 1
+            new_weights[idx] = random.random() * 2 - 1
 
         return Network(new_weights)
+
 
 class GeneticSearcher:
     def __init__(self, pop_size):
         self.pop = [Network.random_network() for i in range(pop_size)]
         self.fitness_cache = {}
         self.nt = NetTester()
+        self.best = None
 
     def rate_network(self, net):
         return self.nt.test_n_times_and_return_min(net, 10)
@@ -47,12 +50,12 @@ class GeneticSearcher:
         population_fitness = [(net, self.fitness(net)) for net in self.pop]
         population_fitness = sorted(population_fitness, reverse=True, key=lambda x: x[1])
         pop_size = len(population_fitness)
-        old_survivors = list(map(lambda x: x[0], population_fitness[:int(pop_size/3)]))
+        old_survivors = list(map(lambda x: x[0], population_fitness[:int(pop_size / 3)]))
         children = []
-        while len(children) < pop_size/3:
+        while len(children) < pop_size / 3:
             parents = random.sample(set(old_survivors), 2)
             children.append(GeneticNetworkHelper.crossing(parents[0], parents[1]))
-        
+
         new_generation = old_survivors + children
 
         while len(new_generation) < 0.9 * pop_size:
@@ -67,42 +70,42 @@ class GeneticSearcher:
         return population_fitness[0][1]
 
     def show_best(self):
-        self.nt.render(self.best)
+        self.nt.render('{}'.format(self.best))
+
 
 class Network:
     def __init__(self, weights):
         self.weights = weights
-    
+
     def __hash__(self):
         return hash(frozenset(self.weights))
 
     def __eq__(self, other):
         return self.weights.__eq__(other.weights)
 
-
     def weighted_sum(self, observation):
-        sum = 0.0
+        s = 0.0
         for i in range(3):
-            sum += self.weights[i] * observation[i]
+            s += self.weights[i] * observation[i]
 
-        return sum + self.weights[3]
+        return s + self.weights[3]
 
     def output(self, observation):
         val = self.weighted_sum(observation) / 2
-#        print('observation: {}, val: {}'.format(observation, val))
         if val > 2:
             return 2
         elif val < -2:
             return -2
-
 
         return val
 
     def __str__(self):
         return str(self.weights)
 
+    @staticmethod
     def random_network():
         return Network([random.random() * 2 - 1 for i in range(4)])
+
 
 class NetTester:
     def __init__(self):
@@ -112,10 +115,9 @@ class NetTester:
         results = [self.test(net) for i in range(n)]
         return min(results)
 
-
     def test(self, net):
         observation = self.env.reset()
-        numpy.ndarray(shape=(1,1), dtype=float)
+        numpy.ndarray(shape=(1, 1), dtype=float)
         action = numpy.array([0])
 
         res = 0.0
@@ -125,7 +127,7 @@ class NetTester:
 
             for i in range(3):
                 observation[i] /= self.env.observation_space.high[i]
-            numpy.ndarray(shape=(1,1), dtype=float)
+            numpy.ndarray(shape=(1, 1), dtype=float)
             action = numpy.array([net.output(observation)])
 
             res += reward
@@ -137,44 +139,26 @@ class NetTester:
 
     def test_with_render(self, net):
         observation = self.env.reset()
-        numpy.ndarray(shape=(1,1), dtype=float)
+        numpy.ndarray(shape=(1, 1), dtype=float)
         action = numpy.array([0])
 
         for t in range(1000):
             self.env.render()
             observation, reward, done, info = self.env.step(action)
-            numpy.ndarray(shape=(1,1), dtype=float)
+            numpy.ndarray(shape=(1, 1), dtype=float)
             action = numpy.array([net.output(observation)])
 
             if done:
                 break
 
-        return t+1
+        return t + 1
 
     def render(self, net):
         val = self.test_with_render(net)
-        print ('result: {}', val)
+        print('result: {}', val)
 
-def main1():
-    env = gym.make('Pendulum-v0')
-    for i_episode in range(20):
-        observation = env.reset()
-        for t in range(100):
-            env.render()
-            print(observation)
-            action = env.action_space.sample()
 
-            numpy.ndarray(shape=(1,1), dtype=float)
-            action=numpy.array([1])
-
-            print(action)
-            print(type(action))
-            observation, reward, done, info = env.step(action)
-            if done:
-                print("Episode finished after {} timesteps".format(t+1))
-                break
-
-def main2():
+def main():
     gs = GeneticSearcher(100)
 
     for i in range(20):
@@ -187,9 +171,5 @@ def main2():
         if best == 10000:
             break
 
-main2()
 
-#print(env.action_space)
-#print(env.observation_space)
-#print(env.observation_space.low)
-#print(env.observation_space.high)
+main()
